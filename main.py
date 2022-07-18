ec231d79a83a153c204dda810d61aa401892b4cb
Dominic Adamo <60450522+DomAdamo@users.noreply.github.com>
2022-07-18 13:04:49 +0000

    Taken directly from computer

--- main.py
+++ main.py
@@ -1,66 +1,61 @@
 import pygame
-import time
+
 pygame.init()
 window = pygame.display.set_mode((500, 500))
-window.fill((125, 125, 125))
-
+window.fill((125,125,125))
 
 def p(g):
-    print(" " + str(g)[1:][:-1].replace("],", "]\n"))
+    print(" "+str(g)[1:][:-1].replace('],',"],\n"))
 
+class Square:
 
-class square:
-    def __init__(self,c,r,val,screen,dire=0,):
-        self.r = r
+    def __init__(self,c,r,v,w,d=0):
         self.c = c
-        self.d = dire
-        self.x = r * 50
-        self.y = c * 50
-        self.val = val
-        self.screen = screen
-        if (self.val != 1):
-            pygame.draw.rect(self.screen, (255, 255, 255),
-                             pygame.Rect(2.5 + self.x, 2.5 + self.y, 45, 45))
-        else:
-            pygame.draw.rect(self.screen, (0, 0, 0),
-                             pygame.Rect(2.5 + self.x, 2.5 + self.y, 45, 45))
+        self.r = r
+        self.val = v
+        self.w=w
+        self.dir = d
+        pygame.draw.rect(self.w, (255, 255, 255), pygame.Rect(2.5+r*50,2.5+ c*50, 45, 45))
 
     def __repr__(self):
         return str(self.val)
-#Added redraw function
-    def redraw(self, v):
-        
-        self.val = v
-        if (self.val != 1):
-            pygame.draw.rect(self.screen, (255, 255, 255),
-                             pygame.Rect(2.5 + self.x, 2.5 + self.y, 45, 45))
+
+    def changeVal(self,nV):
+        self.v = nV
+        if nV  == 1:
+            pygame.draw.rect(self.w, (0, 0, 0), pygame.Rect(2.5+self.r*50,2.5+ self.c*50, 45, 45))
         else:
-            pygame.draw.rect(self.screen, (0, 0, 0),
-                             pygame.Rect(2.5 + self.x, 2.5 + self.y, 45, 45))
-
-        
-
-board = [[square(j, i, 0, window) for i in range(10)] for j in range(10)]
-for i in range(2, 5, 1):
-    board[4][i]=square(4, i, 1, window)
-
-coords = [[4, 2], [4, 4]]
+            pygame.draw.rect(self.w, (255, 255, 255), pygame.Rect(2.5+self.r*50,2.5+ self.c*50, 45, 45))
+    
+board= [[Square(j,i,0,window) for i in range(10)]for j in range(10)]
+board[4][2].changeVal(1)
+board[4][3].changeVal(1)
+board[4][4].changeVal(1)
 
 p(board)
 
+
+
+
+
+vel = 5
+
 run = True
+coords= [[4,2],[4,4]]
+
 while run:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
-            pass
+            print(pygame.key.name(event.key))
+
+    
+    board[coords[0][0]][coords[0][1]].changeVal(0)
+    
     pygame.display.flip()
 
-    #added animation
-    time.sleep(20)
-    
-    board[coords[0][0]][coords[0][1]].redraw(0)
-    
 pygame.quit()
-exit()
+
+
